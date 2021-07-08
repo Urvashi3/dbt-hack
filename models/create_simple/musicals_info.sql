8ac977ee0ca093dd99809fa039b9b163b05f438a
Urvashi Poonia <urvashi.poonia@gmail.com>
2021-07-08 16:59:36 -0500

    Add hack code

--- models/create_simple/musicals_info.sql
+++ models/create_simple/musicals_info.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 
-select e.eventname , e.starttime
+select e.eventname , e.starttime , e.eventid,c.catgroup
 from dev.public.event e inner join dev.public.category c
 on e.catid = c.catid
 where c.catname='Musicals'
